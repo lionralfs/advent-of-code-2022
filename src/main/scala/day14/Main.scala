c705c7016a3bf66f146b4af60fb94c0d989a7cd7
Lion Ralfs <lion.ralfs@gmail.com>
2022-12-14 20:45:28 +0100

    [day14] done

--- src/main/scala/day14/Main.scala
+++ src/main/scala/day14/Main.scala
@@ -3,11 +3,10 @@ package day14
 import utils.Utils._
 
 import scala.collection.mutable
-import scala.util.control.Breaks.break
 
 object Main extends App {
-  def run1(input: List[String]): Int = {
-    val grid = mutable.Map.empty[(Int, Int), String]
+  private def parseCave(input: List[String]): mutable.Map[(Int, Int), String] = {
+    val cave = mutable.Map.empty[(Int, Int), String]
 
     val coordPattern = "(\\d+),(\\d+)".r
     input.foreach(line => {
@@ -16,49 +15,109 @@ object Main extends App {
         .toList
         .sliding(2)
         .foreach({
-          case List(start, end) => {
+          case List(start, end) =>
             val isHorizonalLine = start._2 == end._2
             if (isHorizonalLine) {
               val horizontalCoord = start._2
               val steps = (start._1 - end._1).abs
               val startCoord = start._1.min(end._1)
               for (coord <- startCoord to startCoord + steps) {
-                grid.update((coord, horizontalCoord), "#")
+                cave.update((coord, horizontalCoord), "#")
               }
             } else {
               val verticalCoord = start._1
               val steps = (start._2 - end._2).abs
               val startCoord = start._2.min(end._2)
               for (coord <- startCoord to startCoord + steps) {
-                grid.update((verticalCoord, coord), "#")
+                cave.update((verticalCoord, coord), "#")
               }
             }
-          }
         })
     })
 
-    println(grid)
-    val lowestBlock = grid.keys.map(_._2).max
-    println(lowestBlock)
-    for (iteration <- 1 to 20) {
+    cave
+  }
+
+  def run1(input: List[String]): Int = {
+    val cave = parseCave(input)
+
+    def placeNextSandBlock(cave: mutable.Map[(Int, Int), String], maxHeight: Int): Boolean = {
       var position = (500, 0)
-      while (position._2 < lowestBlock) {
-        val below = (position._1, position._2)
-        println(below)
-        if (!grid.contains(below)) {
-          position = (position._1, position._2 + 1)
+      while (position._2 < maxHeight) {
+        val below = (position._1, position._2 + 1)
+        if (cave.contains(below)) {
+          val leftBelow = (position._1 - 1, position._2 + 1)
+          val rightBelow = (position._1 + 1, position._2 + 1)
+          if (!cave.contains(leftBelow)) {
+            position = leftBelow
+          } else if (!cave.contains(rightBelow)) {
+            position = rightBelow
+          } else {
+            cave.update(position, "o")
+            return true
+          }
         } else {
-          break
+          position = below
         }
+      }
+      false
+    }
+
+    val lowestBlock = cave.keys.map(_._2).max
+    while (true) {
+      val shouldContinue = placeNextSandBlock(cave, lowestBlock)
+      if (!shouldContinue) {
+        return cave.values.count(_ == "o")
+      }
+    }
+    0
+  }
+
+  def run2(input: List[String]): Int = {
+    case class CaveWithFloor(maxHeight: Int, cave: mutable.Map[(Int, Int), String]) {
+      def contains(key: (Int, Int)): Boolean = key._2 == maxHeight + 2 || cave.contains(key)
+
+      def update(key: (Int, Int), value: String): Unit = cave.update(key, value)
+    }
+
+    val cave = parseCave(input)
+
+    def placeNextSandBlock(cave: CaveWithFloor): Boolean = {
+      var position = (500, 0)
+      while (true) {
+        val below = (position._1, position._2 + 1)
+        if (cave.contains(below)) {
+          val leftBelow = (position._1 - 1, position._2 + 1)
+          val rightBelow = (position._1 + 1, position._2 + 1)
+          if (!cave.contains(leftBelow)) {
+            position = leftBelow
+          } else if (!cave.contains(rightBelow)) {
+            position = rightBelow
+          } else if (position == (500, 0)) {
+            cave.update(position, "o")
+            return false
+          } else {
+            cave.update(position, "o")
+            return true
+          }
+        } else {
+          position = below
+        }
+      }
+      false
+    }
+
+    val lowestBlock = cave.keys.map(_._2).max
+    val caveWithFloor = CaveWithFloor(lowestBlock, cave)
+    while (true) {
+      val shouldContinue = placeNextSandBlock(caveWithFloor)
+      if (!shouldContinue) {
+        return cave.values.count(_ == "o")
       }
     }
     1
   }
 
-  def run2(input: List[String]): Int = {
-    1
-  }
-
   println("[Part1]:", run1(readFileByLine("day14-test01.txt")))
-  //  println("[Part2]:", run2(readFileByLine("day14-test01.txt")))
+  println("[Part2]:", run2(readFileByLine("day14-test01.txt")))
 }
--- src/test/scala/day14/Day14Spec.scala
+++ src/test/scala/day14/Day14Spec.scala
@@ -8,18 +8,18 @@ import utils.Utils
 class Day14Spec extends AnyFlatSpec with Matchers {
 
   "Part 1" should "calculate the test input" in {
-    run1(Utils.readFileByLine("day14-test01.txt")) shouldEqual ???
+    run1(Utils.readFileByLine("day14-test01.txt")) shouldEqual 24
   }
 
   it should "calculate the real input" in {
-    run1(Utils.readFileByLine("day14-input.txt")) shouldEqual ???
+    run1(Utils.readFileByLine("day14-input.txt")) shouldEqual 1406
   }
 
   "Part 2" should "calculate the test input" in {
-    run2(Utils.readFileByLine("day14-test01.txt")) shouldEqual ???
+    run2(Utils.readFileByLine("day14-test01.txt")) shouldEqual 93
   }
 
   it should "calculate the real input" in {
-    run2(Utils.readFileByLine("day14-input.txt")) shouldEqual ???
+    run2(Utils.readFileByLine("day14-input.txt")) shouldEqual 20870
   }
 }
