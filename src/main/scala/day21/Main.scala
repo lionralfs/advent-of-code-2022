531bc3f7394b734a00bb39e337b88bf12a2a5039
Lion Ralfs <lion.ralfs@gmail.com>
2022-12-21 18:48:46 +0100

    [day21] part 2 done

--- src/main/scala/day21/Main.scala
+++ src/main/scala/day21/Main.scala
@@ -2,24 +2,38 @@ package day21
 
 import utils.Utils._
 
+import scala.annotation.tailrec
+import scala.collection.mutable
+
 object Main extends App {
-  private def parse(input: List[String]): Map[String, ((Long, Long) => Long, List[String])] = {
+  sealed trait Node {
+    val left: String
+    val right: String
+  }
+
+  private case class OperationNode(operator: String, left: String, right: String) extends Node
+
+  private case class ValueNode(value: Long) extends Node {
+    override val left: String = ""
+    override val right: String = ""
+  }
+
+  private def parse(input: List[String]): Map[String, Node] = {
     input.map {
-      case s"$name: $arg1 + $arg2" => (name, ((add1: Long, add2: Long) => add1 + add2, List(arg1, arg2)))
-      case s"$name: $arg1 - $arg2" => (name, ((add1: Long, add2: Long) => add1 - add2, List(arg1, arg2)))
-      case s"$name: $arg1 * $arg2" => (name, ((add1: Long, add2: Long) => add1 * add2, List(arg1, arg2)))
-      case s"$name: $arg1 / $arg2" => (name, ((add1: Long, add2: Long) => add1 / add2, List(arg1, arg2)))
-      case s"$name: $n" => (name, ((_: Long, _: Long) => n.toLong, Nil))
+      case s"$name: $arg1 $operation $arg2" => (name, OperationNode(operation, arg1, arg2))
+      case s"$name: $n" => (name, ValueNode(n.toLong))
     }.toMap
   }
 
-  private def calc(monkey: String, rules: Map[String, ((Long, Long) => Long, List[String])]): Long = {
-    val (math, dependencies) = rules(monkey)
-    if (dependencies.isEmpty) {
-      math(0, 0)
-    } else {
-      val List(dep1, dep2) = dependencies
-      math(calc(dep1, rules), calc(dep2, rules))
+  private def calc(monkey: String, rules: Map[String, Node]): Long = {
+    rules(monkey) match {
+      case ValueNode(value) => value
+      case OperationNode(operator, left, right) => operator match {
+        case "+" => calc(left, rules) + calc(right, rules)
+        case "-" => calc(left, rules) - calc(right, rules)
+        case "*" => calc(left, rules) * calc(right, rules)
+        case "/" => calc(left, rules) / calc(right, rules)
+      }
     }
   }
 
@@ -30,9 +44,66 @@ object Main extends App {
   }
 
   def run2(input: List[String]): Long = {
-    1
+    val monkeys = parse(input)
+    val hasHuman = mutable.Map.empty[String, Boolean]
+
+    def calcAndFindHuman(monkey: String): (Long, Boolean) = {
+      val result = monkeys(monkey) match {
+        case ValueNode(value) => (value, monkey == "humn")
+        case OperationNode(operator, left, right) =>
+          val (leftResult, hasLeftHuman) = calcAndFindHuman(left)
+          val (rightResult, hasRightHuman) = calcAndFindHuman(right)
+          val hasHuman = hasLeftHuman || hasRightHuman
+          operator match {
+            case "+" => (leftResult + rightResult, hasHuman)
+            case "-" => (leftResult - rightResult, hasHuman)
+            case "*" => (leftResult * rightResult, hasHuman)
+            case "/" => (leftResult / rightResult, hasHuman)
+          }
+      }
+
+      hasHuman.update(monkey, result._2)
+      result
+    }
+
+    @tailrec
+    def calcReverse(monkey: String, target: Long): Long = {
+      monkeys(monkey) match {
+        case ValueNode(value) => if (monkey == "humn") target else value
+        case OperationNode(operator, left, right) =>
+          if (hasHuman(left)) {
+            val rightResult = calc(right, monkeys)
+            operator match {
+              case "+" => calcReverse(left, target - rightResult)
+              case "-" => calcReverse(left, target + rightResult)
+              case "*" => calcReverse(left, target / rightResult)
+              case "/" => calcReverse(left, target * rightResult)
+            }
+          } else if (hasHuman(right)) {
+            val leftResult = calc(left, monkeys)
+            operator match {
+              case "+" => calcReverse(right, target - leftResult)
+              case "-" => calcReverse(right, leftResult - target)
+              case "*" => calcReverse(right, target / leftResult)
+              case "/" => calcReverse(right, leftResult / target)
+            }
+          } else {
+            throw new IllegalStateException()
+          }
+      }
+    }
+
+    val rootNode = monkeys("root")
+    val (leftResult, isHumanLeft) = calcAndFindHuman(rootNode.left)
+    val (rightResult, _) = calcAndFindHuman(rootNode.right)
+
+    if (isHumanLeft) {
+      calcReverse(rootNode.left, rightResult)
+    } else {
+      calcReverse(rootNode.right, leftResult)
+    }
   }
 
   println("[Part1]:", run1(readFileByLine("day21-input.txt")))
-  //  println("[Part2]:", run2(readFileByLine("day21-test01.txt")))
+  println("[Part2]:", run2(readFileByLine("day21-input.txt")))
 }
--- src/test/scala/day21/Day21Spec.scala
+++ src/test/scala/day21/Day21Spec.scala
@@ -15,11 +15,15 @@ class Day21Spec extends AnyFlatSpec with Matchers {
     run1(Utils.readFileByLine("day21-input.txt")) shouldEqual 43699799094202L
   }
 
-//  "Part 2" should "calculate the test input" in {
-//    run2(Utils.readFileByLine("day21-test01.txt")) shouldEqual ???
-//  }
-//
-//  it should "calculate the real input" in {
-//    run2(Utils.readFileByLine("day21-input.txt")) shouldEqual ???
-//  }
+  "Part 2" should "calculate the first test input" in {
+    run2(Utils.readFileByLine("day21-test01.txt")) shouldEqual 301
+  }
+
+  it should "calculate the second test input" in {
+    run2(Utils.readFileByLine("day21-test02.txt")) shouldEqual 19
+  }
+
+  it should "calculate the real input" in {
+    run2(Utils.readFileByLine("day21-input.txt")) shouldEqual 3375719472770L
+  }
 }
